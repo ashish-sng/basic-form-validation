b1d87e44b20747f521fad87b8e384eabf87cbc21
Ashish Singh <ashishsingh.1899@gmail.com>
2023-07-19 19:11:51 +0530

    completed banner part

---
+++ src/components/FormBanner/FormBanner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './FormBanner.css'
+import TitleLogo from './TitleLogo/Logo'
+
+const FormBanner = () => {
+  return (
+    <div className="form__banner"><TitleLogo /></div>
+  )
+}
+
+export default FormBanner
---
+++ src/components/FormBanner/TitleLogo/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Logo.css'
+import bannerLogo from "../../../images/bannerLogo.png"
+
+const Logo = () => {
+  return (
+      <div className="title__logo">
+          <img src={bannerLogo} alt="logo" />
+    </div>
+  )
+}
+
+export default Logo
