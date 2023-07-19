5eba8165f25d6e41ea8a42394d0118829a6f2e08
Ashish Singh <ashishsingh.1899@gmail.com>
2023-07-19 20:30:05 +0530

    made form inputs

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Form.css";
 import formLogo from "../../images/formLogo.png";
+import microsoft from "../../images/microsoftLogo.png";
 
 const Form = () => {
   return (
@@ -9,24 +10,39 @@ const Form = () => {
         <img src={formLogo} alt="logo" />
       </div>
       <hr />
-      <div className="form__container__form">
-        <form>
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            placeholder="Your email.."
-          />
+      <form className="form__container__form">
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Your email.."
+        />
 
-          <label htmlFor="passowrd">Passowrd</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            placeholder="Your password.."
-          />
-        </form>
+        <label htmlFor="passowrd">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Your password.."
+        />
+      </form>
+      <div className="form__container__form__tc">
+        <span>By continuing you agree</span>
+        <span>Forgot password</span>
+      </div>
+      <div className="form__buttons">
+        <button className="signin__button">Sign In</button>
+        <button className="signin__button">Sign in with OTP</button>
+        <button className="signin__button">
+          <img src={microsoft} alt="" /> <span>Login with microsoft</span>
+        </button>
+        <span>We will send you an OTP on your provided contact.</span>
+      </div>
+      <hr />
+      <div className="form__container__form__signup">
+        <span>Don't have an account?</span>
+        <button>Sign up</button>
       </div>
     </div>
   );
